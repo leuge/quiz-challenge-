b93c3afbb2d3729a3210932dc422015ed73651f1
Windy <laureneuge@Laurens-MBP-17.home>
2020-06-22 22:23:33 -0700

    input questions and snswers

--- develop/script.js
+++ develop/script.js
@@ -60,45 +60,45 @@ var myQuestions = [
             3: "for loops",
             4: "console.log", 
         },
-        correctAnswer: "3"
+        correctAnswer: "4"
     },
     {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is",
+        question: "Commonly used data types do NOT include",
         answers: {
-            1: "Javascript",
-            2: "terminal/bash",
-            3: "for loops",
-            4: "console.log", 
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers", 
         },
         correctAnswer: "3"
     },
     {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is",
+    question: "The condition in an if / else statement is enclosed with ___.",
         answers: {
-            1: "Javascript",
-            2: "terminal/bash",
-            3: "for loops",
-            4: "console.log", 
+            1: "quotes",
+            2: "curly brackets",
+            3: "parenthesis",
+            4: "square brackets", 
         },
         correctAnswer: "3"
     },
     {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is",
+    question: "Arrays in JavaScript can be used to store ___.",
         answers: {
-            1: "Javascript",
-            2: "terminal/bash",
-            3: "for loops",
-            4: "console.log", 
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above", 
         },
-        correctAnswer: "3"
+        correctAnswer: "4"
     },
     {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is",
+    question: "String values must be enclosed within ___ when being assigned to variables.",
         answers: {
-            1: "Javascript",
-            2: "terminal/bash",
-            3: "for loops",
-            4: "console.log", 
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis", 
         },
         correctAnswer: "3"
     },
